4ba3a64e777150f810ed20942da63ab009e8ccb5
unknown <alicebe.phan@gmail.com>
2022-01-08 19:23:15 -0600

    Installed ApolloClient, import ApolloClient and ApolloProvider to App.js, wrapped around the router section with ApolloProvider

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: 'graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
+      <Router>
       <>
         <Navbar />
         <Switch>
@@ -15,7 +22,10 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+      </Router>
+
+    </ApolloProvider>
+    
   );
 }
 
