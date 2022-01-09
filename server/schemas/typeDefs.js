dfd87a40335ca9f8cb634736a4c863d1405d5e68
unknown <alicebe.phan@gmail.com>
2022-01-08 20:47:32 -0600

    Added properties to type user and adjusted type query user

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,12 +6,13 @@ const typeDefs = gql`
       username: String!,
       email: String!,
       password: String!,
+      savedBooks: [Book]
 
   }
 
   type Book {
     _id: ID!,
-    authors: String,
+    authors: [String]
     description: String!,
     bookId: String!,
     image: String,
@@ -21,8 +22,8 @@ const typeDefs = gql`
   }
 
   type Query {
-      user: [User]
-      books(_id: String) : [Book]
+      user: User
+      books(_id: be ID!) : [Book]
 
   }
 
