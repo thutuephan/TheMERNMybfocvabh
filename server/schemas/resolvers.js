efdc4ee7172d40d9a9743e7575969773b5748615
unknown <alicebe.phan@gmail.com>
2022-01-09 08:46:29 -0600

    Added type mutation for saveBook

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,12 +16,13 @@ const resolvers = {
     },
 
     Mutation: {
+        // Create user
         addUser: async (parent, args) => {
             const user = await User.create(args);
             const token = signToken(user);
             return { token, user }
         },
-
+        // login
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
@@ -37,11 +38,28 @@ const resolvers = {
 
             const token = signToken(user);
             return { token, user };
-        }
+        },
 
-        saveBook: async (parent, args) => {
+        // save book, add a third argument to the resolver to access data in our context (in this case is bookData)
+        saveBook: async (parent, { bookData}, context) => {
 
-        }
+            // if context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+            if(context.user) {
+                const updatedUser= await User.findOneandUpdate(
+                    { _id: context.user._id},
+                    {
+                        $push: { savedBooks: bookData }
+                    },
+                    { new: true,
+                      runValidation: true 
+                    },
+
+                );
+                return updatedUser;
+            }
+            // if user attempts to execute this mutation and isn't logged in, throw an error
+            throw new AuthenticationError('You need to be logged in');
+        },
 
         
 
