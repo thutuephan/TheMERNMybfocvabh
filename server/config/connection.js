9f38fc37c4cd7ab16414cdfd9cbb3c6997f447b1
unknown <alicebe.phan@gmail.com>
2022-01-12 03:17:44 -0600

    Updated README file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect('mongodb+srv://Alice:PasswordMongo1!@cluster0.mp04l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
