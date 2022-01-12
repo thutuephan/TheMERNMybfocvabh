6f0e6c2db77975b20f5147795292282e058f4203
unknown <alicebe.phan@gmail.com>
2022-01-12 00:28:44 -0600

    still got the 400 error

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -46,23 +46,22 @@ export const SAVE_BOOK = gql`
     }
 
 `;
-
-export const REMOVE_BOOK =gql`
-    mutation removeBook($bookId: String!) {
-        removeBook(bookId: $bookId) {
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!){
+        removeBook(bookId: $bookId){
             _id
             username
-            savedBooks {
+            email
+            savedBooks{
                 bookId
-                title
                 authors
                 description
-                link
                 image
+                link
+                title
             }
         }
     }
-
 `;
 
 
